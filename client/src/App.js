831e5e835b130f56bd7e8d6485fe87e7828f131e
James <jamesschmidt124@gmail.com>
2021-12-10 10:38:26 -0800

    Completed Project

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavBar from './Navbar';
 import LoginPage from "./LoginPage";
-// import './index.css';
+import './index.css';
 import WatchPage from "./WatchPage";
 
 
--- client/src/WatchCard.js
+++ client/src/WatchCard.js
@@ -1,11 +1,11 @@
 function WatchCard({watch:{name, price, description, imageurl}}){
     return(
-        <>
+        <div class= "card">
     <h2>Price in $ {price}</h2>
     <img src= {imageurl}/>
     <div>{name}</div>
     <p>{description}</p>
-        </>
+        </div>
     )
 }
 export default WatchCard
